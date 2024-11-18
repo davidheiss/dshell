f845a469a9a6b3880a408a7468a52299485afebd
David Heiß <david.heiss0406@gmail.com>
2024-11-18 18:06:54 +0100

    start with workspace groupings

--- src/widgets/panel/workspaces.py
+++ src/widgets/panel/workspaces.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 import cairo
 from cairo import Context
 from gi.repository import Gtk, GLib
@@ -5,11 +6,12 @@ from services import Hyprland
 from enum import Enum
 import math
 import datetime
+import itertools
 
 from windows.panel import HEIGHT
 
 WORKSPACE_COUNT = 5
-WORKSPACE_SIZE = 16
+WORKSPACE_SIZE = 14
 WORKSPACE_SPACING = 4
 
 
@@ -34,7 +36,7 @@ class Animation:
         widget: Gtk.DrawingArea,
         duration: datetime.timedelta,
         value: float = 0,
-        timing=easeInOutBack,
+        timing=easeInSine,
     ):
         self.widget = widget
         self.duration = duration
@@ -63,13 +65,60 @@ class Animation:
         return progress < 1
 
 
-class Workspace:
-    def __init__(self, active: bool, empty: bool):
-        self.active = active
-        self.empty = empty
+class Group(set[int]):
+    def __init__(self, ids: Iterable[int]):
+        super().__init__(ids)
+        pass
 
-    def __repr__(self):
-        return f"Workspace({self.active},{self.empty})"
+    def __contains__(self, key: int):
+        return min(self) - 1 <= key <= max(self) + 1 if len(self) > 0 else False
+
+    def can_split(self):
+        for i,j in itertools.pairwise(self):
+            if i + 1 != j:
+                return True
+        return False
+
+    def split(self):
+        return Groups(self)
+
+
+    def draw(self, context: cairo.Context, width, height):
+        if len(self) == 0:
+            return
+
+        start = workspace_pos(min(self))
+        end = workspace_pos(max(self))
+        y = height / 2
+
+        context.arc(start, y, WORKSPACE_SIZE, math.pi * 0.5, math.pi * 1.5)
+        context.arc(end, y, WORKSPACE_SIZE, math.pi * 1.5, math.pi * 0.5)
+        context.set_source_rgb(0.4, 0.6, 0.8)
+        context.fill()
+
+class Groups(list[Group]):
+    def __init__(self, iterable: Iterable[int]):
+        super().__init__()
+        for id in iterable:
+            for group in self:
+                if id in group:
+                    group.add(id)
+                    break
+            else:
+                self.append(Group([id]))
+
+    def create(self, widget: Gtk.DrawingArea, id: int | str):
+        widget.queue_draw()
+
+    def destory(self, widget: Gtk.DrawingArea, id: int | str):
+        widget.queue_draw()
+
+def workspace_pos(id: int | str):
+    return (
+        (int(id) - 1) * (WORKSPACE_SPACING + WORKSPACE_SIZE * 2)
+        + WORKSPACE_SPACING
+        + WORKSPACE_SIZE
+    )
 
 
 class Workspaces(Gtk.DrawingArea):
@@ -85,11 +134,14 @@ class Workspaces(Gtk.DrawingArea):
         active_id = Hyprland.active_workspace().id
         self.active_animation = Animation(
             self,
-            datetime.timedelta(milliseconds=300),
+            datetime.timedelta(milliseconds=150),
             value=(active_id - 1) * (WORKSPACE_SPACING + WORKSPACE_SIZE * 2)
             + WORKSPACE_SPACING
             + WORKSPACE_SIZE,
         )
+
+        workspace_ids = list(map(lambda x: x.id, Hyprland.workspaces()))
+        self.groups = Groups(workspace_ids)
 
         hyprland = Hyprland.instance()
         hyprland.connect("workspacev2", self.on_workspace)
@@ -97,26 +149,36 @@ class Workspaces(Gtk.DrawingArea):
         hyprland.connect("destroyworkspacev2", self.on_destroy_workspace)
 
     def on_workspace(self, hyprland: Hyprland, id: str, name: str):
-        self.active_animation.animate(
-            (int(id) - 1) * (WORKSPACE_SPACING + WORKSPACE_SIZE * 2)
-            + WORKSPACE_SPACING
-            + WORKSPACE_SIZE,
-        )
+        self.active_animation.animate(workspace_pos(id))
 
     def on_create_workspace(self, hyprland: Hyprland, id: str, name: str):
-        pass
+        self.groups.create(self, id)
 
     def on_destroy_workspace(self, hyprland: Hyprland, id: str, name: str):
-        pass
+        self.groups.destory(self, id)
 
     def on_draw(self, _, context: Context, width: int, height: int):
-        # context.rectangle(0, 0, width, height)
-        # context.set_source_rgb(1, 0, 0)
-        # context.fill()
-
         y = height / 2
 
-        context.new_path()
+        for group in self.groups:
+            group.draw(context, width, height)
+
+        x = WORKSPACE_SIZE + WORKSPACE_SPACING
+        for i in range(1, WORKSPACE_COUNT + 1):
+
+            context.select_font_face("Arial")
+            context.set_font_size(18)
+            context.set_source_rgb(1, 1, 1)
+            text_extends = context.text_extents(str(i))
+            context.move_to(
+                x - text_extends.x_bearing - text_extends.width / 2,
+                y + text_extends.height / 2,
+            )
+            context.show_text(str(i))
+            context.fill()
+
+            x += WORKSPACE_SIZE * 2 + WORKSPACE_SPACING
+
         context.arc(
             self.active_animation.value,
             y,
@@ -124,7 +186,10 @@ class Workspaces(Gtk.DrawingArea):
             0,
             math.pi * 2,
         )
-        context.set_source_rgb(0, 1, 0)
+        context.clip()
+
+        context.rectangle(0, 0, width, height)
+        context.set_source_rgb(0.6, 0.8, 1.0)
         context.fill()
 
         x = WORKSPACE_SIZE + WORKSPACE_SPACING
@@ -132,15 +197,15 @@ class Workspaces(Gtk.DrawingArea):
 
             context.new_path()
             context.select_font_face("Arial")
-            context.set_font_size(18)
-            context.set_source_rgb(1, 1, 1)
+            context.set_font_size(16)
+            context.set_source_rgb(0, 0, 0)
             text_extends = context.text_extents(str(i))
             context.move_to(
-                x - text_extends.x_bearing * 2 - text_extends.width / 2,
+                x - text_extends.x_bearing - text_extends.width / 2,
                 y + text_extends.height / 2,
             )
             context.show_text(str(i))
-            context.stroke()
+            context.fill()
 
             x += WORKSPACE_SIZE * 2 + WORKSPACE_SPACING
 
